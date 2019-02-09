1176906659e64e6718de3c5bda3702d0af1a2e0a
pyneng <pyneng.course@gmail.com>
2019-02-09 10:55:39 +0000

    check ip functions

---
+++ examples/09_functions/check_ip_functions.py
@@ -0,0 +1,23 @@
+import ipaddress
+import time
+
+def check_ip(ip_address, verbose=False):
+    try:
+        ipaddress.ip_interface(ip_address)
+        return True
+    except ValueError as err:
+        if verbose: print(err)
+        return False
+
+
+def return_correct_ip(ip_addresses):
+    correct = []
+    for ip in ip_addresses:
+        if check_ip(ip):
+            correct.append(ip)
+    return correct
+
+if __name__ == "__main__":
+    result = check_ip('10.1.1.1/24')
+    time.sleep(5)
+    print('Function result:', result)
