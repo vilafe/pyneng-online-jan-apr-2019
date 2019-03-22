73795dc70bdcf2fbe7a77cc5aabae598b88c21d4
pyneng <pyneng.course@gmail.com>
2019-03-22 20:59:55 +0000

    Добавила примеры для лекции

--- examples/20_concurrent_connections/netmiko_threads_submit_logging.py
+++ examples/20_concurrent_connections/netmiko_threads_submit_logging.py
@@ -1,27 +1,38 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from pprint import pprint
 from datetime import datetime
+from pprint import pprint
 import time
 from itertools import repeat
 import threading
+import logging
 
 
 import yaml
 from netmiko import ConnectHandler
+
+
+# отключаем логи paramiko:
+logging.getLogger("paramiko").setLevel(logging.WARNING)
+
+logging.basicConfig(
+    format='%(threadName)s %(name)s %(levelname)s: %(message)s',
+    level=logging.INFO
+)
+
 
 start_msg = '===> {} Connection to device: {}'
 received_msg = '<=== {} Received result from device: {}'
 
 
 def connect_ssh(device_dict, command):
-    #print(f'Thread: {threading.current_thread()} running... Device: {device_dict["ip"]}')
-    print(start_msg.format(datetime.now().time(), device_dict['ip']))
+    #logging.info(f'Thread: {threading.current_thread()} running... Device: {device_dict["ip"]}')
+    logging.info(start_msg.format(datetime.now().time(), device_dict['ip']))
     if device_dict['ip'] == '192.168.100.1':
         time.sleep(10)
     with ConnectHandler(**device_dict) as ssh:
         ssh.enable()
         result = ssh.send_command(command)
-        print(received_msg.format(datetime.now().time(), device_dict['ip']))
+        logging.info(received_msg.format(datetime.now().time(), device_dict['ip']))
     return {device_dict['ip']: result}
 
 
@@ -37,7 +48,8 @@ def threads_conn(function, devices, limit=2, command=''):
 
 
 if __name__ == '__main__':
-    devices = yaml.load(open('devices.yaml'))
+    with open('devices.yaml') as f:
+        devices = yaml.load(f, Loader=yaml.FullLoader)
     all_done = threads_conn(
         connect_ssh, devices['routers'], command='sh clock')
     pprint(all_done)
