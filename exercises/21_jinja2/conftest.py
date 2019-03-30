0fa4aee33dcb2430048cd5aa211e91255db4341f
pyneng <pyneng.course@gmail.com>
2019-03-30 10:20:12 +0000

    Задания и тесты 21_jinja2

---
+++ exercises/21_jinja2/conftest.py
@@ -0,0 +1,38 @@
+import re
+import yaml
+import pytest
+from netmiko import ConnectHandler
+
+
+@pytest.fixture(scope='module')
+def three_routers_from_devices_yaml():
+    with open('devices.yaml') as f:
+        devices = yaml.load(f)
+        devices = devices[:3]
+        options = {'timeout': 5, 'fast_cli': True}
+        for device in devices:
+            device.update(options)
+    return devices
+
+
+@pytest.fixture(scope='module')
+def r1_r2_r3_test_connection(three_routers_from_devices_yaml):
+    connections = []
+    for params in three_routers_from_devices_yaml:
+        router = ConnectHandler(**params)
+        router.enable()
+        connections.append(router)
+    yield connections
+    for r in connections:
+        r.disconnect()
+
+
+def strip_empty_lines(output):
+    lines = []
+    for line in output.strip().split('\n'):
+        line = line.strip()
+        if line:
+            lines.append(re.sub(' +', ' ', line.strip()))
+    return '\n'.join(lines)
+
+
