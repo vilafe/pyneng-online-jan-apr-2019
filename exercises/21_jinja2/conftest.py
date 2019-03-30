b57a7db46fbe64d80e658c1b5f766cfcb074008d
Наташа Самойленко <pyneng.course@gmail.com>
2019-03-30 15:29:09 +0200

    Удалила лишние fixtures

--- exercises/21_jinja2/conftest.py
+++ exercises/21_jinja2/conftest.py
@@ -1,31 +1,6 @@
 import re
 import yaml
 import pytest
-from netmiko import ConnectHandler
-
-
-@pytest.fixture(scope='module')
-def three_routers_from_devices_yaml():
-    with open('devices.yaml') as f:
-        devices = yaml.load(f)
-        devices = devices[:3]
-        options = {'timeout': 5, 'fast_cli': True}
-        for device in devices:
-            device.update(options)
-    return devices
-
-
-@pytest.fixture(scope='module')
-def r1_r2_r3_test_connection(three_routers_from_devices_yaml):
-    connections = []
-    for params in three_routers_from_devices_yaml:
-        router = ConnectHandler(**params)
-        router.enable()
-        connections.append(router)
-    yield connections
-    for r in connections:
-        r.disconnect()
-
 
 def strip_empty_lines(output):
     lines = []
